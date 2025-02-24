f15e9766cd429bd8305c8a7f9e16c797dd54a297
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-24 16:20:31 -0500

    feat: Expressions examples

---
+++ src/main/scala/lectures/Expressions.scala
@@ -0,0 +1,31 @@
+package lectures
+
+object Expressions extends App {
+  //Expressions
+  // + - * / & | ^ << >> >>> (Right shift with zero extension)
+  // == != > >= < <=
+  // ! && ||
+  // += -= *= /=
+  val x = 1 + 2
+  println(x)
+
+  // + - * / & | ^ << >> >>> (Right shift with zero extension)
+  //Example:
+  println(2 + 3 * 4)
+
+  // == != > >= < <=
+  //Example:
+  println(1 == x)
+
+  // ! && ||
+  //Example:
+  println(true && false)
+  println(true || false)
+
+  // += -= *= /=
+  //Example:
+  var y = 5
+   y += 5
+  println(y)
+
+}
