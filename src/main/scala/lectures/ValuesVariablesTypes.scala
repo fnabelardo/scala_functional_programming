764a8d512d96251beb0c01b131ab1e2e0cf9affe
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-24 15:17:03 -0500

    feat: Define values (immutable)

---
+++ src/main/scala/lectures/ValuesVariablesTypes.scala
@@ -0,0 +1,15 @@
+package lectures
+
+object ValuesVariablesTypes extends App {
+  //Values (Immutable)
+  val x: Int = 42
+  println(x)
+
+  val aString: String = "Noel"
+  val aBoolean: Boolean =  false
+  val aChar: Char = 'n'
+  val aShort: Short = 4158
+  val aLong: Long = 5478965412457869L
+  val aFloat: Float = 5.0f
+  val aDouble: Double = 6.15
+}
