8dbd57d7596402d23e5b1c243bbd5563b46d03ed
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-25 11:38:00 -0500

    feat: Using recursive function instead a loop

--- src/main/scala/lectures/Functions.scala
+++ src/main/scala/lectures/Functions.scala
@@ -8,4 +8,11 @@ object Functions extends App {
 
   println(aFunction("Hello", 5))
 
+  //Use recursive function when I need a loop
+  def aRepeatedFunction(aString: String, n: Int): String = {
+    if (n == 1) aString
+    else aString + aRepeatedFunction(aString, n - 1)
+  }
+  println(aRepeatedFunction("Greetings ", 3))
+
 }
