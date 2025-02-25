990a1894afda67901042025f8fa47938816de6d1
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-25 12:40:38 -0500

    feat: Create Fibonacci function

---
+++ src/main/scala/lectures/FibonacciFunction.scala
@@ -0,0 +1,20 @@
+package lectures
+
+object FibonacciFunction extends App {
+  /*
+  *  3. Fibonacci function.
+  * Ex: f(1) = 1
+  * *** f(2) = 2
+  * *** f(n) = f( n - 1 ) +  f( n - 2 )
+  * */
+  private def fibonacciFunction(number: Int): BigInt = {
+    if (number == 1 || number == 2) {
+      1
+    } else {
+      fibonacciFunction(number - 1) + fibonacciFunction(number - 2)
+    }
+  }
+
+  println(fibonacciFunction(8))//Output: 21
+
+}
