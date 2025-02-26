004a2af5efdf5f9e917f0c954080f2f9f5a57420
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-25 19:38:21 -0500

    feat: Use tail recursion to repeat string

---
+++ src/main/scala/lectures/ConcatenateString.scala
@@ -0,0 +1,17 @@
+package lectures
+
+import scala.annotation.tailrec
+
+object ConcatenateString extends App {
+  private def concatenateTailRecursion(aRepeatNumber: Int, aString: String): String = {
+    @tailrec
+    def helpConcatenateString(n: Int, a: String, accumulator: String): String = {
+      if (n <= 0) accumulator
+      else helpConcatenateString(n - 1, a, a + accumulator)
+    }
+
+    helpConcatenateString(aRepeatNumber, aString, "")
+  }
+
+  println(concatenateTailRecursion(3, "Noel "))
+}
