7dc5534aa20cfffd72b495954778ed1e1369cfd9
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-27 17:45:19 -0500

    feat: Create Counter class

--- src/main/scala/lectures/object_oriented_programming/Novel.scala
+++ src/main/scala/lectures/object_oriented_programming/Novel.scala
@@ -8,6 +8,13 @@ object Novel extends App {
 
   println(novel.authorAge)
   println(novel.isWrittenBy(author))
+
+  val counter = new Counter
+//  println(counter.increment.print())
+  println(counter.increment(3).print())
+
+//  println(counter.decrement.print())
+//  println(counter.decrement(3).print())
 }
 
 class Writer(firstName: String, surname: String, val yearOfBirth: Int) {
@@ -16,6 +23,32 @@ class Writer(firstName: String, surname: String, val yearOfBirth: Int) {
 
 class Novel(name: String, yearOfRelease: Int, author: Writer) {
   def authorAge = LocalDateTime.now().getYear - author.yearOfBirth
+
   def isWrittenBy(author: Writer) = this.author == author
+
   def copy(newYearOfRelease: Int): Novel = new Novel(name, newYearOfRelease, author)
+}
+
+class Counter(val count: Int = 0) {
+  def increment = {
+    println("incrementing")
+    new Counter(count + 1)
+  }
+
+  def decrement = {
+    println("decrementing")
+    new Counter(count - 1)
+  }
+
+  def increment(value: Int): Counter = {
+    if (value <= 0) this
+    else increment.increment(value - 1)
+  }
+
+  def decrement(value: Int): Counter = {
+    if (value <= 0) this
+    else increment.decrement(value - 1)
+  }
+
+  def print() = println(count)
 }
