8947117fc8b7243196a1c817675eca75eb9ecd17
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-26 19:01:50 -0500

    feat: Define arguments by default

---
+++ src/main/scala/lectures/DefaultArgs.scala
@@ -0,0 +1,16 @@
+package lectures
+
+import scala.annotation.tailrec
+
+object DefaultArgs extends App {
+  @tailrec
+  private def trFact(n: Int, acc: Int = 1): Int ={
+    if (n <= 1) acc
+    else trFact(n - 1, n * acc)
+  }
+
+  private val trFact3 = trFact(3, 1 )
+  private val trFact4 = trFact(4)
+  println(trFact3)
+  println(trFact4)
+}
