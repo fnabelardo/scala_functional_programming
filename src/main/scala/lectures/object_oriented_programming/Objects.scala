e3471c4ba6018e6898e087739b5ababeec4b6080
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-01 18:37:44 -0500

    feat: Define factory method to object person

--- src/main/scala/lectures/object_oriented_programming/Objects.scala
+++ src/main/scala/lectures/object_oriented_programming/Objects.scala
@@ -6,23 +6,33 @@ object Objects extends App {
     //Static/class - level functionalities
     val N_EYES = 2
     def canFly: Boolean = false
+
+    //Factory method
+    def apply(mother: Person, father: Person): Person = new Person("Jonny Jr.")
   }
-  class Person {
+  class Person(val name: String) {
     //Instance level functionalities
   }
   //Companions: object Person and class Person
+  // - Can acces each other's provate members
+  // - Scala is OO
 
   println(Person.N_EYES)
   println(Person.canFly)
 
   //Scala object = Singleton instance
   val messi = new Person("Messi")
-  val john = new Person("John")
-  println(messi == john) //Output: false
+  val antonela = new Person("Antonela")
+  println(messi == antonela) //Output: false
 
   val person1 = Person
   val person2 = Person
   println(person1 == person2) //Output: true
 
-  println()
+  //Factory method
+  val jonny = Person(messi, antonela)//Equivalent: Person.apply(messi, antonela)
+
+  //Scala Applications = Scala object with
+  // def main(args: Array[String]): Unit
+
 }
