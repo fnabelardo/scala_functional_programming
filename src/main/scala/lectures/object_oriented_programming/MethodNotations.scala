8e22edb232012e24cb95fd4cb7700bfbf8bd2587
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-01 15:24:28 -0500

    feat: Use operators in Scala. Postfix notation

--- src/main/scala/lectures/object_oriented_programming/MethodNotations.scala
+++ src/main/scala/lectures/object_oriented_programming/MethodNotations.scala
@@ -1,11 +1,17 @@
 package lectures.object_oriented_programming
 
+import scala.language.postfixOps
+
 object MethodNotations extends App {
   case class Person(val name: String, favoriteMovie: String) {
     def likes(movie: String): Boolean = movie == favoriteMovie
+    //Infix notation
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with $person"
     def +(person: Person): String = s"${this.name} is adding with $person"
+    //Prefix notation
     def unary_! : String = s"$name, what the heck?!"
+    //Postfix notation
+    def isAlive: Boolean = true //This function do not receive any parameter
   }
 
   val jane = new Person("Jane", "Inception")
@@ -28,4 +34,9 @@ object MethodNotations extends App {
 
   println(jane.unary_!)//Output: Jane, what the heck?!
   println(!jane)//Output: Jane, what the heck?! //Equivalent with println(jane.unary_!)
+
+  //Postfix notation
+  println(jane isAlive)//Equivalent with println(jane.isAlive)
+  println(jane.isAlive)
+
 }
