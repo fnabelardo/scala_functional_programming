3fdf7a839e082cae759947ff278a9f17892a5b8f
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-01 15:18:59 -0500

    feat: Use operators in Scala. Prefix notation

--- src/main/scala/lectures/object_oriented_programming/MethodNotations.scala
+++ src/main/scala/lectures/object_oriented_programming/MethodNotations.scala
@@ -5,6 +5,7 @@ object MethodNotations extends App {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with $person"
     def +(person: Person): String = s"${this.name} is adding with $person"
+    def unary_! : String = s"$name, what the heck?!"
   }
 
   val jane = new Person("Jane", "Inception")
@@ -19,4 +20,12 @@ object MethodNotations extends App {
   println(jane hangOutWith tommy)
   println(jane.name + tommy.name)
   println(jane.name + tommy)
+
+  //Prefix notation (Syntactic sugar)
+  val x = -1 //Equivalent with 1.unary_-
+  val y = 1.unary_-
+  //unary_prefix only works with - + ~ !
+
+  println(jane.unary_!)//Output: Jane, what the heck?!
+  println(!jane)//Output: Jane, what the heck?! //Equivalent with println(jane.unary_!)
 }
