974999b57ffc1f93a101c06504937c8a14a6ec4b
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-03 11:52:14 -0500

    feat: Create single list integer list

---
+++ src/main/scala/lectures/object_oriented_programming/MyList.scala
@@ -0,0 +1,31 @@
+package lectures.object_oriented_programming
+
+abstract class MyList {
+  /* Single link integer list
+  * head = first element of the list
+  * tail = remainder of the list
+  * isEmpty = is this list empty
+  * add(int) = new list with this element added
+  * toString = a string representation of the list * */
+  def head: Int
+  def tail: MyList
+  def isEmpty: Boolean
+  def add(element: Int): MyList
+  //  def toString: String
+}
+
+object EmptyList extends MyList {
+  def head: Int = throw new NoSuchElementException
+  def tail: MyList = throw new NoSuchElementException
+  def isEmpty: Boolean = true
+  def add(element: Int): MyList = new ConstructionList(element, EmptyList)
+}
+
+class ConstructionList(h: Int, t: MyList) extends MyList {
+  def head: Int = h
+  def tail: MyList = t
+  def isEmpty: Boolean = false
+  def add(element: Int): MyList = new ConstructionList(element, this)
+}
+
+
