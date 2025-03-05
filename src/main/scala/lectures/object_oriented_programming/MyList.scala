f3e0f7350ec32f3448f3a4656df4ca1ac61451a8
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-04 19:05:22 -0500

    feat: Convert list into generic list

--- src/main/scala/lectures/object_oriented_programming/MyList.scala
+++ src/main/scala/lectures/object_oriented_programming/MyList.scala
@@ -1,52 +1,44 @@
 package lectures.object_oriented_programming
 
-abstract class MyList {
+abstract class MyList[+A] {
   /* Singled link integer list
   * head = first element of the list
   * tail = remainder of the list
   * isEmpty = is this list empty
   * add(int) = new list with this element added
   * toString = a string representation of the list * */
-  def head: Int
-  def tail: MyList
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
   def printElements: String
   override def toString: String = "[" + printElements + "]"
 }
 
-object EmptyList extends MyList {
-  def head: Int = throw new NoSuchElementException
-  def tail: MyList = throw new NoSuchElementException
+object EmptyList extends MyList[Nothing]{
+  def head: Nothing = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(element: Int): MyList = new ConstructionList(element, EmptyList)
+  def add[B >: Nothing](element: B): MyList[B] = new ConstructionList(element, EmptyList)
   def printElements: String = ""
 }
 
-class ConstructionList(h: Int, t: MyList) extends MyList {
-  def head: Int = h
-  def tail: MyList = t
+class ConstructionList[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
+  def tail: MyList[A] = t
   def isEmpty: Boolean = false
-  def add(element: Int): MyList = new ConstructionList(element, this)
+  def add[B >: A](element: B): MyList[B] = new ConstructionList(element, this)
   def printElements: String =
     if(t.isEmpty) s"$h"
     else s"$h ${t.printElements}"
 }
 
 object ListTest extends App {
-  //Create list with one element
-  val list = new ConstructionList(1, EmptyList)
-  println(list.head)
+  val listOfIntegers: MyList[Int] = new ConstructionList(1, ConstructionList(2, ConstructionList(3, EmptyList)))
+  val listOfStrings: MyList[String] = new ConstructionList("Hello", new ConstructionList("Scala", EmptyList))
 
-  //Create list with 3 element
-  val list2 = new ConstructionList(1, ConstructionList(2, ConstructionList(3, EmptyList)))
-  println(s"List 2 head: ${list2.head}")
-  println(s"List 2 tail head: ${list2.tail.head}")
-  println(s"List 2 tail head: ${list2.tail.head}")
-
-  //Polymorphic call
-  println(list2.toString)
-
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
 }
 
 
