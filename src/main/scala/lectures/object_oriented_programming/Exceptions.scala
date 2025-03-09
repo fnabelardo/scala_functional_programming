d424411fcff55938022e7739e34ec710d5f1aeb5
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-09 14:47:08 -0400

    feat: Crash the program with an OutOfMemoryError

--- src/main/scala/lectures/object_oriented_programming/Exceptions.scala
+++ src/main/scala/lectures/object_oriented_programming/Exceptions.scala
@@ -19,23 +19,30 @@ object Exceptions extends App {
   }
 
   val potentialFall = try {
-      //Code that might throw
-      getInt(false)
-    } catch {
-      case e: RuntimeException => 43
-    } finally {
+    //Code that might throw
+    getInt(false)
+  } catch {
+    case e: RuntimeException => 43
+  } finally {
     //Code that will get executed NO MATTER WHAT
     //Optional
     //Does not influence the return type of the expression
     //Use finally only for side effects
-      println("Finally")
-    }
+    println("Finally")
+  }
 
-  println(potentialFall)//Output: 43
+  println(potentialFall) //Output: 43
 
   //3. How to define own exceptions
   class MyException extends Exception
+
   val exception = new MyException
 
-  //throw exception
+  /*
+    * 1. Crash the program with an OutOfMemoryError
+   */
+  //val array = Array.ofDim[Int](Int.MaxValue, 4)//Output: Exception in thread "main" java.lang.OutOfMemoryError: Requested array size exceeds VM limit
+
+  //println(PocketCalculator.add(Int.MaxValue, 10))
+
 }
