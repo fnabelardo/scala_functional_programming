b01b219bad68cb83278ae3026467584c0283cb0a
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-11 13:31:24 -0400

    feat: Define curried function

--- src/main/scala/lectures/functional_programming/WhatsAFunction.scala
+++ src/main/scala/lectures/functional_programming/WhatsAFunction.scala
@@ -21,14 +21,35 @@ object WhatsAFunction extends App {
 
   //Function types Function2[A, B, R] === (A,B) => R
   //All Scala functions are objects
-  /*
 
+  /*
+  * 1. A function that takes 2 strings and concatenate them
+  * 2. Transform the MyPredicate and MyTransformer into function types
+  * 3. Define a function which takes an int and returns another
+  *     function which takes an int and returns an int
+  *     - What's the type of this function
+  *     - How to do it
+  * */
+
+  /*
   * A function that takes 2 strings and concatenate them
   */
   val concat: (String, String) => String = new Function2[String, String, String] {
     override def apply(a: String, b: String): String = a + b
   }
   println(concat("Hello", " World"))
+
+  // Function1[Int, Function1[Int, Int]]
+  //Curried Function: Function that takes multiple arguments one at a time.
+  val superAdder: Function1[Int, Function1[Int, Int]] = new Function1[Int, Function1[Int, Int]] {
+    override def apply(x: Int): Function1[Int, Int] = new Function[Int, Int] {
+      override def apply(y: Int): Int = x + y
+    }
+  }
+
+  val adder3 = superAdder(3)
+  println(adder3(4)) //Output: 7
+  println(superAdder(3)(4)) //Output: 7
 
 }
 
