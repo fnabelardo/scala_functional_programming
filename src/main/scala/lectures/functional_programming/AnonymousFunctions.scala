450e8c641841d6e9fa95418ee45d05e9876c6cb4
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-11 17:06:31 -0400

    feat: Define Anonymous function (LAMBDA)

---
+++ src/main/scala/lectures/functional_programming/AnonymousFunctions.scala
@@ -0,0 +1,14 @@
+package lectures.functional_programming
+
+object AnonymousFunctions extends App {
+
+  /*val doubler = new Function[Int, Int] {
+    override def apply(x: Int) = x * 2
+  } //Equivalent to --- val doubler = (x: Int) => x * 2 */
+
+  //Anonymous function (LAMBDA)
+  //val doubler: Int => Int = (x: Int) => x * 2 //Equivalent to ---> val doubler: Int => Int = x => x * 2
+  //val doubler: Int => Int = x => x * 2        //Equivalent to ---> val doubler = (x: Int) => x * 2
+  val doubler = (x: Int) => x * 2
+
+}
