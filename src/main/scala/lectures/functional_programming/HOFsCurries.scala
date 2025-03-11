cc41102eb965814fc73b6477120ff07c8bcd8a3b
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-11 18:47:59 -0400

    feat: Define function that applies a function n times over a value x

---
+++ src/main/scala/lectures/functional_programming/HOFsCurries.scala
@@ -0,0 +1,17 @@
+package lectures.functional_programming
+
+object HOFsCurries extends App {
+  val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = null
+  //Higher order function (HOF) -> Examples: map, flatMap, filter in MyList
+
+  //Function that applies a function n times over a value x
+  //nTimes(f, n, x)
+  //nTimes(f, 3, x) = f(f(f(x)))
+  def nTimes(f: Int => Int, n: Int, x: Int): Int = {
+    if (n <= 0) x
+    else nTimes(f, n - 1, f(x))
+  }
+
+  val plusOne = (x: Int) => x + 1
+  println(nTimes(plusOne, 10, 1))
+}
