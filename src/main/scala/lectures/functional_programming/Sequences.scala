bab030e85b4438f1c47fb1312edf8bc8830f01b7
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-13 16:37:40 -0400

    feat: Test ranges

--- src/main/scala/lectures/functional_programming/Sequences.scala
+++ src/main/scala/lectures/functional_programming/Sequences.scala
@@ -11,4 +11,12 @@ object Sequences extends App {
   println(aSequence ++ Seq(5,6,7))
   println(aSequence.sorted)
 
+  //Ranges
+  val aRange: Seq[Int] = 1 to 10
+  println("---aRange---")
+  aRange.foreach(println)
+  val aRange2: Seq[Int] = 1 until 10
+  println("---aRange2---")
+  aRange2.foreach(println)
+
 }
