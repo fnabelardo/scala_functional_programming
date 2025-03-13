d420fc48b7203931bf05a561c74f57365c29d59e
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-13 16:50:36 -0400

    feat: Test lists

--- src/main/scala/lectures/functional_programming/Sequences.scala
+++ src/main/scala/lectures/functional_programming/Sequences.scala
@@ -19,4 +19,20 @@ object Sequences extends App {
   println("---aRange2---")
   aRange2.foreach(println)
 
+  //Lists
+  val aList = List(1,2,3)
+  val prependedList = 42 :: aList //Prepended
+  val prependedList2 = 43 +: aList //Prepended
+  val appendedList = aList :+ 15 //Appended
+  println("---prependedList---")
+  println(prependedList) //Output: List(42, 1, 2, 3)
+  println(prependedList2) //Output: List(43, 1, 2, 3)
+  println("---appendedList---")
+  println(appendedList) //Output: List(1, 2, 3, 15)
+
+  val apples = List.fill(3)("apples")
+  println("---apples---")
+  println(apples) //Output: List(apples, apples, apples)
+  println(aList.mkString("-|-")) //Output: 1-|-2-|-3
+
 }
