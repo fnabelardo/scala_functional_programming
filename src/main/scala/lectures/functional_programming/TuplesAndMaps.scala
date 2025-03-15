2387b6445161dcc3abff0119cdf57d976e6cc1a2
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-15 11:22:24 -0400

    feat: Define Tuple

---
+++ src/main/scala/lectures/functional_programming/TuplesAndMaps.scala
@@ -0,0 +1,9 @@
+package lectures.functional_programming
+
+object TuplesAndMaps extends App {
+
+  //Tuples = finite ordered "lists"
+  val tuple = new Tuple2(2, "hello, scala") //Tuple2[Int, String] = (Int, String)
+  val aTuple = (2, "hello, scala")
+
+}
