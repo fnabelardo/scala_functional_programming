ac78d778c6cfda4483d810af5b4f9866419acb37
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-15 17:31:26 -0400

    feat: Remove friendship

--- src/main/scala/lectures/functional_programming/TuplesAndMaps.scala
+++ src/main/scala/lectures/functional_programming/TuplesAndMaps.scala
@@ -85,5 +85,12 @@ object TuplesAndMaps extends App {
     network + (personA -> (friendsA + personB)) + (personB -> (friendsB + personA))
   }
 
+  def unfriend(network: Map[String, Set[String]], personA: String, personB: String): Map[String, Set[String]] = {
+    val friendsA = network(personA)
+    val friendsB = network(personB)
+
+    network + (personA -> (friendsA - personB)) + (personB -> (friendsB - personA))
+  }
+
   }
 
