3eace749f3146d3df76b67f1d93c5213d91b404a
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-03-15 11:27:23 -0400

    feat: Work with Tuples

--- src/main/scala/lectures/functional_programming/TuplesAndMaps.scala
+++ src/main/scala/lectures/functional_programming/TuplesAndMaps.scala
@@ -6,4 +6,15 @@ object TuplesAndMaps extends App {
   val tuple = new Tuple2(2, "hello, scala") //Tuple2[Int, String] = (Int, String)
   val aTuple = (2, "hello, scala")
 
+  //Working with Tuples
+  //  Access to tuple element
+  println("--Access to tuple element--")
+  println(aTuple._1)
+  // Copy a tuple
+  println("--Copy--")
+  println(aTuple.copy(_2 = " goodbye, Rails"))
+  // Swap a tuple
+  println("--Swap a tuple--")
+  println(aTuple.swap)
+
 }
